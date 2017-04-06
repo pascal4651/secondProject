d73bea750d4f4a822ba7737b1df680d0cd17c6fb
Oleksandr Shvets <pascal36@yandex.com>
2017-04-06 20:58:00 +0200

    added vs program

---
+++ TryBranchesProject/TryBranchesProject/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryBranchesProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 1;
+            int y = 2;
+            Console.WriteLine("Result=" + x + y);
+            Console.ReadKey();
+        }
+    }
+}
